0928c95e040999745a057b8f684aca98a68c6995
Novita Diastuti <novita895@gmail.com>
2020-01-21 17:24:03 +0700

    add index.php + Dockerfile

---
+++ src/index.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Hello, World from Docker! <br>";
+echo "Learn to use Kubernetes by Novita Diastuti! <br>";
+echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
+
+?>
